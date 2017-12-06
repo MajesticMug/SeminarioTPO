d383dc7b12fef00123cab9f49ddeb844dd760bae
Unknown <gastons315@gmail.com>
2017-12-06 19:55:33 -0300

    Menu PPAL V1

--- src/vista/AltaTurno.java
+++ src/vista/AltaTurno.java
@@ -1,14 +1,28 @@
 package vista;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import com.toedter.calendar.JDateChooser;
+
+
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
-import javax.swing.JLabel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
 
-public class AltaTurno
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class AltaTurno extends JFrame
 {
 
 	private JFrame frmAltaTurno;
@@ -51,10 +65,70 @@ public class AltaTurno
 		frmAltaTurno.setTitle("Alta Turno");
 		frmAltaTurno.setBounds(100, 100, 450, 300);
 		frmAltaTurno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmAltaTurno.getContentPane().setLayout(null);
 		
-		JLabel lblFecha = new JLabel("Fecha:");
-		lblFecha.setBounds(10, 11, 46, 14);
-		frmAltaTurno.getContentPane().add(lblFecha);
+		JPanel contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		frmAltaTurno.setContentPane(contentPane);
+		
+		JDateChooser dateChooser = new JDateChooser();
+		dateChooser.setBounds(158, 126, 97, 20);
+		contentPane.add(dateChooser);
+		
+		JComboBox hora = new JComboBox();
+		hora.setModel(new DefaultComboBoxModel(new Integer[] {10, 11, 12, 13, 14, 15, 16, 17}));
+		hora.setBounds(158, 155, 97, 22);
+		contentPane.add(hora);
+		
+		JComboBox minutos = new JComboBox();
+		minutos.setModel(new DefaultComboBoxModel(new Integer[] {0, 30}));
+		minutos.setBounds(158, 188, 97, 22);
+		contentPane.add(minutos);
+		
+		JLabel lblAltaTurno = new JLabel("Alta Turno");
+		lblAltaTurno.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblAltaTurno.setBounds(176, 11, 79, 20);
+		contentPane.add(lblAltaTurno);
+		
+		JLabel lblProfesional = new JLabel("Profesional");
+		lblProfesional.setBounds(43, 52, 66, 14);
+		contentPane.add(lblProfesional);
+		
+		JLabel lblPaciente = new JLabel("Paciente");
+		lblPaciente.setBounds(43, 87, 46, 14);
+		contentPane.add(lblPaciente);
+		
+		JLabel lblDia = new JLabel("Dia");
+		lblDia.setBounds(43, 126, 46, 14);
+		contentPane.add(lblDia);
+		
+		JLabel lblHora = new JLabel("Hora");
+		lblHora.setBounds(43, 159, 46, 14);
+		contentPane.add(lblHora);
+		
+		JLabel lblMinuto = new JLabel("Minuto");
+		lblMinuto.setBounds(43, 189, 46, 14);
+		contentPane.add(lblMinuto);
+		
+		JComboBox comboBoxProfesional = new JComboBox();
+		comboBoxProfesional.setBounds(158, 48, 97, 22);
+		contentPane.add(comboBoxProfesional);
+		
+		JComboBox comboBoxPaciente = new JComboBox();
+		comboBoxPaciente.setBounds(158, 83, 97, 22);
+		contentPane.add(comboBoxPaciente);
+		
+		JButton btnAceptar_1 = new JButton("Aceptar");
+		btnAceptar_1.setBounds(164, 239, 91, 23);
+		contentPane.add(btnAceptar_1);
+		btnAceptar_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Date date = dateChooser.getDate();
+				//int prueba = (int) hora.getSelectedItem();
+				date.setHours((int) hora.getSelectedItem());
+				date.setMinutes((int) minutos.getSelectedItem());
+				System.out.println("DATE CHOOSER "+date.toString());
+			}
+		});
 	}
 }
--- src/vista/MenuPrincipal.java
+++ src/vista/MenuPrincipal.java
@@ -3,12 +3,16 @@ package vista;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import java.awt.Panel;
 
 public class MenuPrincipal {
 	//PRUEBA ARAM
 	//Prueba Gas
 	//Prueba REni
-	private JFrame frame;
+	private JFrame frmCentroOdontolgicoIntegral;
 
 	/**
 	 * Launch the application.
@@ -18,7 +22,7 @@ public class MenuPrincipal {
 			public void run() {
 				try {
 					MenuPrincipal window = new MenuPrincipal();
-					window.frame.setVisible(true);
+					window.frmCentroOdontolgicoIntegral.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -37,9 +41,52 @@ public class MenuPrincipal {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmCentroOdontolgicoIntegral = new JFrame();
+		frmCentroOdontolgicoIntegral.setTitle("Centro Odontol\u00F3gico Integral");
+		frmCentroOdontolgicoIntegral.setBounds(100, 100, 450, 300);
+		frmCentroOdontolgicoIntegral.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmCentroOdontolgicoIntegral.getContentPane().setLayout(null);
+		
+		JButton btnPacientes = new JButton("Pacientes");
+		btnPacientes.setBounds(10, 36, 89, 23);
+		frmCentroOdontolgicoIntegral.getContentPane().add(btnPacientes);
+		
+		JButton btnEsecialistas = new JButton("Esecialistas");
+		btnEsecialistas.setBounds(109, 36, 89, 23);
+		frmCentroOdontolgicoIntegral.getContentPane().add(btnEsecialistas);
+		
+		JButton btnTurnos = new JButton("Turnos");
+		btnTurnos.setBounds(204, 36, 89, 23);
+		frmCentroOdontolgicoIntegral.getContentPane().add(btnTurnos);
+		
+		JButton btnHistoriasClnicas = new JButton("Historias Cl\u00EDnicas");
+		btnHistoriasClnicas.setBounds(10, 147, 111, 23);
+		frmCentroOdontolgicoIntegral.getContentPane().add(btnHistoriasClnicas);
+		
+		JButton btnRadiografas = new JButton("Radiograf\u00EDas");
+		btnRadiografas.setBounds(123, 147, 89, 23);
+		frmCentroOdontolgicoIntegral.getContentPane().add(btnRadiografas);
+		
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.setBounds(215, 147, 89, 23);
+		frmCentroOdontolgicoIntegral.getContentPane().add(btnBuscar);
+		
+		JButton btnInformacin = new JButton("Informaci\u00F3n");
+		btnInformacin.setBounds(308, 147, 89, 23);
+		frmCentroOdontolgicoIntegral.getContentPane().add(btnInformacin);
+		
+		Panel panel = new Panel();
+		panel.setBounds(308, 36, 116, 82);
+		frmCentroOdontolgicoIntegral.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmCentroOdontolgicoIntegral.setJMenuBar(menuBar);
+		
+		JMenuItem mntmArchivo = new JMenuItem("Archivo");
+		menuBar.add(mntmArchivo);
+		
+		JMenuItem mntmAcercaDe = new JMenuItem("Acerca de");
+		menuBar.add(mntmAcercaDe);
 	}
-
 }
