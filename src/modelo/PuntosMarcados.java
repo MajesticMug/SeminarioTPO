1d035a08139f9c238cbc575ab9018226fbe79618
amorcecian <amorcecian@gmail.com>
2017-12-12 00:23:40 -0300

    Arreglo clase Hibernate

--- src/modelo/PuntosMarcados.java
+++ src/modelo/PuntosMarcados.java
@@ -2,6 +2,7 @@ package modelo;
 
 import java.util.List;
 
+import javax.persistence.*;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,7 +11,7 @@ import javax.persistence.GenerationType;
 @Entity
 public class PuntosMarcados
 {
-	@javax.persistence.Id
+	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int Id;
 	
