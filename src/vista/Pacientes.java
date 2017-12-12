502db8972073f82ffa507a98b187089311d0ad67
Unknown <gastons315@gmail.com>
2017-12-12 01:27:25 -0300

    Se agrega lógica a botones de Paciente

--- src/vista/Pacientes.java
+++ src/vista/Pacientes.java
@@ -7,6 +7,12 @@ import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controlador.Sistema;
+import modelo.Odontologo;
+import modelo.Paciente;
+import modelo.Radiologo;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
@@ -47,8 +53,8 @@ public class Pacientes extends JFrame {
 		}
 		return inst;
 	}
-	
-	
+
+
 	/**
 	 * Create the frame.
 	 */
@@ -60,66 +66,91 @@ public class Pacientes extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblAgregarNuevoPaciente = new JLabel("Agregar Nuevo Paciente");
 		lblAgregarNuevoPaciente.setBounds(10, 11, 214, 14);
 		contentPane.add(lblAgregarNuevoPaciente);
-		
+
 		txtNombre = new JTextField();
 		txtNombre.setToolTipText("Nombre");
 		txtNombre.setText("Nombre");
 		txtNombre.setBounds(10, 36, 214, 20);
 		contentPane.add(txtNombre);
 		txtNombre.setColumns(10);
-		
+
 		txtApellido = new JTextField();
 		txtApellido.setToolTipText("Apellido");
 		txtApellido.setText("Apellido");
 		txtApellido.setBounds(10, 61, 214, 20);
 		contentPane.add(txtApellido);
 		txtApellido.setColumns(10);
-		
+
 		txtTipoDni = new JTextField();
 		txtTipoDni.setToolTipText("Tipo DNI");
 		txtTipoDni.setText("Tipo DNI");
 		txtTipoDni.setBounds(10, 85, 86, 20);
 		contentPane.add(txtTipoDni);
 		txtTipoDni.setColumns(10);
-		
+
 		txtNmero = new JTextField();
 		txtNmero.setToolTipText("N\u00FAmero");
 		txtNmero.setText("N\u00FAmero");
 		txtNmero.setBounds(101, 86, 123, 20);
 		contentPane.add(txtNmero);
 		txtNmero.setColumns(10);
-		
+
 		txtDireccin = new JTextField();
 		txtDireccin.setToolTipText("Direcci\u00F3n");
 		txtDireccin.setText("Direcci\u00F3n");
 		txtDireccin.setBounds(10, 108, 214, 20);
 		contentPane.add(txtDireccin);
 		txtDireccin.setColumns(10);
-		
+
 		txtTelfono = new JTextField();
 		txtTelfono.setToolTipText("Tel\u00E9fono");
 		txtTelfono.setText("Tel\u00E9fono");
-		txtTelfono.setBounds(10, 139, 214, 20);
+		txtTelfono.setBounds(10, 128, 214, 20);
 		contentPane.add(txtTelfono);
 		txtTelfono.setColumns(10);
-		
+
 		JTextPane textPane = new JTextPane();
 		textPane.setToolTipText("Detalle del Paciente");
 		textPane.setBounds(10, 170, 291, 81);
 		contentPane.add(textPane);
-		
+
 		JButton btnAgregar = new JButton("Agregar");
+		btnAgregar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				Paciente p = null;
+
+				p.setApellido(txtApellido.getText());
+				p.setApellido(txtNombre.getText());
+				p.setTipoDocumento(txtTipoDni.getText());
+				p.setNroDocumento( Integer.parseInt(txtNmero.getText()));
+				p.setDomicilio(txtDireccin.getText());
+				p.setTelefono(Integer.parseInt(txtTelfono.getText()));
+
+				Sistema.getInstance().agregarPaciente(p);
+
+			}
+		});
 		btnAgregar.setBounds(322, 159, 89, 23);
 		contentPane.add(btnAgregar);
-		
+
 		JButton btnEliminar = new JButton("Eliminar");
-		btnEliminar.setBounds(322, 204, 89, 23);
-		contentPane.add(btnEliminar);
+		btnEliminar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				Paciente pac = Sistema.getInstance().buscarPaciente(Integer.parseInt(txtNmero.getText()));
+				if (pac == null){ Sistema.getInstance().eliminarPaciente(pac);}
+				
+			}
+		});
 		
+		btnEliminar.setBounds(322, 193, 89, 23);
+		contentPane.add(btnEliminar);
+
 		JButton btnVolver = new JButton("Volver");
 		btnVolver.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -132,19 +163,15 @@ public class Pacientes extends JFrame {
 		});
 		btnVolver.setBounds(322, 228, 89, 23);
 		contentPane.add(btnVolver);
-		
-		JButton btnModificar = new JButton("Modificar");
-		btnModificar.setBounds(322, 182, 89, 23);
-		contentPane.add(btnModificar);
-		
+
 		JPanelConFondo panelImagen = new JPanelConFondo();
 		panelImagen.setImagen(Toolkit.getDefaultToolkit().getImage(MenuPrincipal.class.getResource("/resources/img/Paciente1.jpg")));
 		panelImagen.setBounds(265, 34, 123, 114);
 		panelImagen.setLayout(null);
 		contentPane.add(panelImagen);
-		
-		
-		
+
+
+
 	}
 
 }
